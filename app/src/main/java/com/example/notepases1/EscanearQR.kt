cdcbcbda267977e59bf0be38855c8e7f31b66743
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2024-03-03 13:27:14 -0500

    Escanear QR - XML

---
+++ app/src/main/java/com/example/notepases1/EscanearQR.kt
@@ -0,0 +1,13 @@
+package com.example.notepases1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EscanearQR : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_escanear_qr)
+    }
+
+}
