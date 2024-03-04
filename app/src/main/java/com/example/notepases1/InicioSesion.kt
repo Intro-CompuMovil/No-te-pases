f412ff4571025e97df670caa735088f7b4451981
linasalamanca <118871999+linasalamanca@users.noreply.github.com>
2024-03-04 11:11:15 -0500

    menu

--- app/src/main/java/com/example/notepases1/InicioSesion.kt
+++ app/src/main/java/com/example/notepases1/InicioSesion.kt
@@ -28,7 +28,7 @@ class InicioSesion : AppCompatActivity() {
 
         ingreso.setOnClickListener{
             if(validarCuenta(usuario, contraseña)){
-                val intentMenu = Intent(this, EscanearQR::class.java)
+                val intentMenu = Intent(this, Menu::class.java)
                 startActivity(intentMenu)
             }
         }
--- app/src/main/java/com/example/notepases1/Menu.kt
+++ app/src/main/java/com/example/notepases1/Menu.kt
@@ -1,11 +1,30 @@
 package com.example.notepases1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Menu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        val textRecargar = findViewById<TextView>(R.id.textRecargar)
+        val textQR = findViewById<TextView>(R.id.textQR)
+        val textNumPasajeros = findViewById<TextView>(R.id.textNumPasajeros)
+
+        textRecargar.setOnClickListener{
+            val intentRecargar = Intent(this, RecargarCuenta::class.java)
+            startActivity(intentRecargar)
+        }
+        textQR.setOnClickListener {
+            val intentQR = Intent(this, EscanearQR::class.java)
+            startActivity(intentQR)
+        }
+       /* textNumPasajeros.setOnClickListener {
+            val intentNumPasajeros = Intent(this, )
+        }*/
+
     }
 }
