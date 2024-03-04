21676a5d04ceca79c7a7095c0192bc51dfdb4834
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2024-03-03 20:26:08 -0500

    Informacion Bus - Avances básicos

--- app/src/main/java/com/example/notepases1/InformacionBus.kt
+++ app/src/main/java/com/example/notepases1/InformacionBus.kt
@@ -2,10 +2,25 @@ package com.example.notepases1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class InformacionBus : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_informacion_bus)
+
+        val cantPasajeros = findViewById<TextView>(R.id.numPasajeros)
+        val distanciaParadero = findViewById<TextView>(R.id.kmDistancia)
+
+        //Verificar con Santiago cómo me va a pasar la información de los buses
+        val informacionBundle = intent.getBundleExtra("infoCategoria")
+        val informacionIntent = intent.getStringExtra("info")
+        val categoria = informacionBundle?.getString("categoria")
+
+        //calcular la cantidad de pasajeros
+        //cantPasajeros.text =
+
+        //asignar la distancia
+        //distanciaParadero.text =
     }
 }
