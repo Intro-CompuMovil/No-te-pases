86a58c703e705200123f8707e83f6fb337e81a69
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2024-04-15 18:57:15 -0500

    Mapa ubicaciones buses sensor luz

--- app/src/main/java/com/example/notepases1/Mapa.kt
+++ app/src/main/java/com/example/notepases1/Mapa.kt
@@ -1,27 +1,46 @@
 package com.example.notepases1
 
+import android.app.UiModeManager
+import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.util.Log
 import com.example.notepases1.databinding.ActivityMapaBinding
 import org.osmdroid.api.IMapController
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.TilesOverlay
 
 class Mapa : AppCompatActivity() {
 
     private lateinit var bindingMapa: ActivityMapaBinding
+    private lateinit var sensorManejador: SensorManager
+    private lateinit var sensorLuz: Sensor
+    private lateinit var sensorLuzListener: SensorEventListener
 
     private val startPoint = org.osmdroid.util.GeoPoint(4.628593, -74.065041)
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        sensorManejador = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorLuz = sensorManejador.getDefaultSensor(Sensor.TYPE_LIGHT)!!
+        sensorLuzListener = createLightSensorListener()
+        sensorManejador.registerListener(
+            sensorLuzListener,
+            sensorLuz,
+            SensorManager.SENSOR_DELAY_FASTEST
+        )
+
         val ctx = applicationContext
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
         Configuration.getInstance().userAgentValue = packageName
@@ -34,58 +53,34 @@ class Mapa : AppCompatActivity() {
         bindingMapa.osmMap.setTileSource(TileSourceFactory.MAPNIK)
         bindingMapa.osmMap.setMultiTouchControls(true)
 
-        ubicaciones()
+        ubicaciones(0,4.628593, -74.065041, true)
+        ubicaciones(1,4.626404, -74.065593, false)
+        ubicaciones(2,4.622692, -74.066504, false)
+        ubicaciones(3,4.617333, -74.068570, false)
     }
 
-    private fun ubicaciones(){
-        val puntoInicio = startPoint
-        val punto1 = GeoPoint(4.626404, -74.065593)
-        val punto2 = GeoPoint(4.622692, -74.066504)
-        val punto3 = GeoPoint(4.617333, -74.068570)
+    private fun ubicaciones(id: Int, latitud: Double, longitud: Double, inicio: Boolean){
+        val punto = GeoPoint(latitud, longitud)
 
-        val marcadorInicio = Marker(bindingMapa.osmMap)
-        val marcador1 = Marker(bindingMapa.osmMap)
-        val marcador2 = Marker(bindingMapa.osmMap)
-        val marcador3 = Marker(bindingMapa.osmMap)
+        val marcador = Marker(bindingMapa.osmMap)
 
-        val iconoInicio = cambioTamañoIcono(resources.getDrawable(R.drawable.ubicacion))
-        val icono1 = cambioTamañoIcono(resources.getDrawable(R.drawable.iconobus))
-        val icono2 = cambioTamañoIcono(resources.getDrawable(R.drawable.iconobus))
-        val icono3 = cambioTamañoIcono(resources.getDrawable(R.drawable.iconobus))
-
-        marcadorInicio.icon = iconoInicio
-        marcador1.icon = icono1
-        marcador2.icon = icono2
-        marcador3.icon = icono3
-
-        marcadorInicio.position = puntoInicio
-        marcador1.position = punto1
-        marcador2.position = punto2
-        marcador3.position = punto3
-
-        marcadorInicio.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        bindingMapa.osmMap.overlays.add(marcadorInicio)
-
-        marcador1.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        bindingMapa.osmMap.overlays.add(marcador1)
-
-        marcador2.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        bindingMapa.osmMap.overlays.add(marcador2)
-
-        marcador3.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        bindingMapa.osmMap.overlays.add(marcador3)
-
-        marcador1.setOnMarkerClickListener { _, _ ->
-            cambiarActividad(1, 30)
+        if (inicio){
+            marcador.icon = cambioTamañoIcono(resources.getDrawable(R.drawable.ubicacion))
+        } else {
+            marcador.icon = cambioTamañoIcono(resources.getDrawable(R.drawable.iconobus))
         }
 
-        marcador2.setOnMarkerClickListener { _, _ ->
-            cambiarActividad(2, 40)
+        marcador.position = punto
+
+        marcador.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+        bindingMapa.osmMap.overlays.add(marcador)
+
+        if(!inicio){
+            marcador.setOnMarkerClickListener { _, _ ->
+                cambiarActividad(id, 30)
+            }
         }
 
-        marcador3.setOnMarkerClickListener { _ , _ ->
-            cambiarActividad(3, 10)
-        }
     }
 
     private fun cambiarActividad(id: Int, cantidadP: Int): Boolean{
@@ -104,12 +99,37 @@ class Mapa : AppCompatActivity() {
         return BitmapDrawable(resources, bitmapCambiado)
     }
 
+    private fun createLightSensorListener(): SensorEventListener {
+    return object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            if (bindingMapa.osmMap != null) {
+                if (event.values[0] < 3000) {
+                    Log.i("MAPS", "DARK MAP " + event.values[0])
+                    bindingMapa.osmMap.overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+                } else {
+                    Log.i("MAPS", "LIGHT MAP " + event.values[0])
+                    bindingMapa.osmMap.overlayManager.tilesOverlay.setColorFilter(null)
+                }
+            }
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
+    }
+}
+
     override fun onResume() {
         super.onResume()
         bindingMapa.osmMap.onResume()
         val mapController: IMapController = bindingMapa.osmMap.controller
         mapController.setZoom(18.0)
         mapController.setCenter(this.startPoint)
+
+        /*val uiManager = getSystemService(Context.UI_MODE_SERVICE) as UiModeManager
+        if(uiManager.nightMode == UiModeManager.MODE_NIGHT_YES)
+            bindingMapa.osmMap.overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+        */
+
+
     }
     override fun onPause() {
         super.onPause()
